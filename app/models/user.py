12156fe321fd492813ffecb0b974e8a2ae78b8bc
Mothilal Jadhav <mothilaljadav0304@gmail.com>
2026-08-16 11:40:04 +0530

    User object

---
+++ app/models/user.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from app.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    user_id = Column(Integer, primary_key=True, index=True)
+    role_id = Column(Integer, ForeignKey("roles.id", ondelete="SET NULL"))
+    user_name = Column(String(50), nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    password_hash = Column(String(255), nullable=False)
+    created_at = Column(DateTime, nullable=False)
+
+    role = relationship("Role")
+
+
+    
