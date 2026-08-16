790085684051d82f38e75057c2cbdd7e54d239f5
Mothilal Jadhav <mothilaljadav0304@gmail.com>
2026-08-16 14:24:56 +0530

    hash functions

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, func
 from sqlalchemy.orm import relationship
 from app.database import Base
 
@@ -7,13 +7,20 @@ class User(Base):
     __tablename__ = "users"
 
     user_id = Column(Integer, primary_key=True, index=True)
-    role_id = Column(Integer, ForeignKey("roles.id", ondelete="SET NULL"))
+
+    role_id = Column(
+        Integer,
+        ForeignKey("roles.id", ondelete="SET NULL")
+    )
+
     user_name = Column(String(50), nullable=False)
     email = Column(String(100), unique=True, nullable=False)
     password_hash = Column(String(255), nullable=False)
-    created_at = Column(DateTime, nullable=False)
+
+    created_at = Column(
+        DateTime,
+        nullable=False,
+        server_default=func.current_timestamp()
+    )
 
     role = relationship("Role")
-
-
-    
