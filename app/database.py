4cf3988ee2b3f6df6d4a76e93948dae6f349a822
Mothilal Jadhav <mothilaljadav0304@gmail.com>
2026-08-16 11:27:15 +0530

    databse connection

---
+++ app/database.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+
+database_url = "mysql+pymysql://root:mothilal%4012@127.0.0.1:3306/Task_management_system"
+
+engine = create_engine(database_url)
+
+SessionLocal = sessionmaker(
+    autocommit = False,
+    autoflush= False,
+    bind=engine
+)
+
+Base = declarative_base()
