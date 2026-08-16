5f07ef76bbcec27d45f84fae99dcf044c18f7f8c
Mothilal Jadhav <mothilaljadav0304@gmail.com>
2026-08-16 14:06:00 +0530

    route for post method which validates the email, adds the user)

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.database import get_db
 from app.models.user import User
-from app.schemas.user import UserResponse
+from app.schemas.user import UserResponse, UserCreate
 
 
 
@@ -15,5 +15,31 @@ router = APIRouter(
 @router.get("/",response_model=list[UserResponse])
 
 def get_users(db: Session = Depends(get_db)):
-    users = db.query(User).all
+    users = db.query(User).all()
     return users
+
+@router.post("/", response_model=UserResponse, status_code=201)
+def create_user( user_data: UserCreate, db: Session = Depends(get_db)):
+    existing_user = db.query(User).filter(User.email == user_data.email).first()
+
+    if existing_user:
+        raise HTTPException(
+            status_code=409,
+            detail = "emaill already registered"
+        )
+
+    user = User(
+        user_name = user_data.user_name,
+        email = user_data.email,
+        password_hash = user_data.password,
+        role_id = user_data.role_id
+    )
+
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+
+    return user
+
+
+
